93d19cbddb075a115b7376c63eab86019368fa5c
Gustavo Valvassori <faogustavo@gmail.com>
2021-06-18 22:19:14 -0300

    created dotfiles repo

---
+++ fish/config.fish
@@ -0,0 +1,51 @@
+source ~/.asdf/asdf.fish
+. ~/.asdf/plugins/java/set-java-home.fish
+
+set -gx ANDROID_HOME "/Users/faogustavo/Library/Android/sdk"
+set -gx PATH $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin /Users/faogustavo/.bin $PATH
+
+# git aliases
+abbr -a gd "git diff -M"
+abbr -a ga "git add"
+abbr -a gaa "git add --all ."
+abbr -a gbd "git branch -D"
+abbr -a gst "git status"
+abbr -a gca "git commit -am"
+abbr -a gm "git merge --no-ff"
+abbr -a gpt "git push --tags"
+abbr -a gp "git push"
+abbr -a grh "git reset --hard"
+abbr -a gb "git branch"
+abbr -a gcob "git checkout -b"
+abbr -a gco "git checkout"
+abbr -a gba "git branch -a"
+abbr -a gcp "git cherry-pick"
+abbr -a gl "git log --pretty=format:\"%Cgreen%h%Creset - %Cblue%an%Creset @ %ar : %s\""
+abbr -a gl2 "git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
+abbr -a glv "git log --stat"
+abbr -a gpom "git pull origin master"
+abbr -a gcd 'cd "`git rev-parse --show-toplevel`"'
+abbr -a gcf "git clean -fd"
+abbr -a gcod "git checkout -- ."
+abbr -a gpum "git pull upstream master"
+abbr -a gpod "git push origin --delete"
+abbr -a gsu "git status -uno"
+abbr -a gcm "git commit -m"
+abbr -a gcv "git commit --verbose"
+abbr -a gc "git commit --verbose"
+abbr -a gds "git diff | sublime"
+abbr -a grm "git reset HEAD"
+abbr -a gacm "git add . --all; git commit --verbose"
+abbr -a gtd "git log --tags --simplify-by-decoration --pretty=\"format:%ai %d\""
+abbr -a grs "git shortlog -s -n --all --no-merges"
+
+alias branch_name="git branch | grep \* | cut -d ' ' -f2"
+alias ggpush="git push -u origin (branch_name)"
+alias gwip='git add --all; git commit -m "wip" --no-verify'
+alias gunwip='git log -n 1 | grep -q -c wip; and git reset HEAD~1'
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+  alias llt "ll --tree --level 2 -a"
+end
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Other tools
+
+- Fish Shell: https://fishshell.com/
+  - OhMyFish: https://github.com/oh-my-fish/oh-my-fish
+  - Fisher: https://github.com/jorgebucaran/fisher
+
+- Z: https://github.com/jethrokuan/z
+
+- ASDF: https://asdf-vm.com/#/core-manage-asdf
+  - ASDF Java: https://github.com/halcyon/asdf-java
+  - ASDF Node: https://github.com/asdf-vm/asdf-nodejs
+  - ASDF Ruby: https://github.com/asdf-vm/asdf-ruby
+
+- Brew: https://brew.sh/index_pt-br
+
+- Github CLI: https://github.com/cli/cli
+
+- PidCat: https://github.com/JakeWharton/pidcat
+
+- Boop: https://github.com/IvanMathy/Boop
+
+- Kap: https://getkap.co/
+
+- Jetbrains Toolbox: https://www.jetbrains.com/pt-pt/lp/toolbox/
+
+- Notion: https://www.notion.so
+
+- Vysor: https://www.vysor.io/
+
+- NordVPN: https://nordvpn.com/pt-br
+
+- Grammarly: https://app.grammarly.com/
+
+- One Password: https://1password.com/
